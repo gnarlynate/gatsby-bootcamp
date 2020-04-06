6bead2a9546dcca99f157bcf28f18e58e510d718
Nathen Bowes <nathen.bowes@shopify.com>
2020-04-06 12:25:24 -0700

    create query for blog page content

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,11 +1,42 @@
 import React from 'react'
+import { graphql, useStaticQuery } from 'gatsby'
 import Layout from '../components/layout'
 
 const BlogPage = () => {
+  const data = useStaticQuery(graphql`
+    query{
+      allMarkdownRemark{
+        edges {
+          node {
+            id
+            frontmatter{
+              title
+              date
+            }
+          }
+        }
+      }
+    }
+  `)
+
+  const posts = data.allMarkdownRemark.edges
+
   return (
     <Layout>
       <h1>Blog</h1>
       <p>Posts will show up later on.</p>
+      <ol>
+        {
+          posts.map(post => {
+            return (
+              <li key={post.node.id}>
+                <h2>{post.node.frontmatter.title}</h2>
+                <p>{post.node.frontmatter.date}</p>
+              </li>
+            )
+          })
+        }
+      </ol>
     </Layout>
   )
 }
