7de6e0954e68655ad57ae8df5cfd07f99cd750ec
Nathen Bowes <nathen.bowes@shopify.com>
2020-04-07 13:45:47 -0700

    add styles for footer and blog components

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -2,6 +2,8 @@ import React from 'react'
 import { graphql, useStaticQuery, Link } from 'gatsby'
 import Layout from '../components/layout'
 
+import blogStyles from './blog.module.scss'
+
 const BlogPage = () => {
   const data = useStaticQuery(graphql`
     query{
@@ -27,13 +29,18 @@ const BlogPage = () => {
   return (
     <Layout>
       <h1>Blog</h1>
-      <ol>
+      <ol className={blogStyles.posts}>
         {
           posts.map(post => {
             return (
-              <li key={post.node.id}>
-                <h2><Link to={`/blog/${post.node.fields.slug}`}>{post.node.frontmatter.title}</Link></h2>
-                <p>{post.node.frontmatter.date}</p>
+              <li 
+                key={post.node.id}
+                className={blogStyles.post}
+              >
+                  <Link to={`/blog/${post.node.fields.slug}`}>
+                    <h2>{post.node.frontmatter.title}</h2>
+                    <p>{post.node.frontmatter.date}</p>
+                  </Link>
               </li>
             )
           })
